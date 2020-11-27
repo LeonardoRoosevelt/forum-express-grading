e9f1609adf78cc79c57787b39abb9275c6232061
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-11-27 10:23:23 +0800

    Install pg and update production config

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const db = require('./models') // 引入資料庫
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
+
 const session = require('express-session')
 const passport = require('./config/passport')
 const app = express()
@@ -18,6 +19,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
 app.use('/upload', express.static(__dirname + '/upload'))
+
 // 把 req.flash 放到 res.locals 裡面
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
