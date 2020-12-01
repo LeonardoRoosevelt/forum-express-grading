126fefdfc938f5ef710f03a5ca0a83e1e44bd4c7
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-12-01 17:12:36 +0800

    Add selected option condition to category select

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' })) // Handlebars 註冊樣板引擎
+app.engine('hbs', exphbs({ defaultLayout: 'main', helpers: require('./config/handlebars-helpers'), extname: 'hbs' })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'hbs') // 設定使用 Handlebars 做為樣板引擎
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
