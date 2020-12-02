9acf7b86a49bf1a595165958d75b082f8941e7a9
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-12-02 16:33:33 +0800

    Convert time format using dayjs

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -1,8 +1,14 @@
+const dayjs = require('dayjs')
+const relativeTime = require('dayjs/plugin/relativeTime')
 module.exports = {
-  ifCond: function (a, b, options) {
+  ifCond: function(a, b, options) {
     if (a === b) {
       return options.fn(this)
     }
     return options.inverse(this)
+  },
+  dayjs: function(a) {
+    dayjs.extend(relativeTime)
+    return dayjs(a).fromNow()
   }
 }
