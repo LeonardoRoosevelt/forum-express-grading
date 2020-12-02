b449426c933f105ed588b1f697036ca6cce46788
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-12-02 12:24:45 +0800

    Add restaurants show page

--- routes/index.js
+++ routes/index.js
@@ -25,31 +25,32 @@ module.exports = (app, passport) => {
     }
     res.redirect('/signin')
   }
-  //如果使用者訪問首頁，就導向 /restaurants 的頁面
+
   app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
-  //在 /restaurants 底下則交給 restController.getRestaurants 來處理
+
   app.get('/restaurants', authenticated, restController.getRestaurants)
-  // 連到 /admin 頁面就轉到 /admin/restaurants
-  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
-  // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
-  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
+  app.get('/restaurants/:id', authenticated, restController.getRestaurant)
+
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
   app.get('/signin', userController.signInPage)
-  app.post(
-    '/signin',
+  app.post('/signin',
     passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }),
-    userController.signIn
-  )
+    userController.signIn)
   app.get('/logout', userController.logout)
+
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
   app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
   app.post('/admin/restaurants', authenticatedAdmin, upload.single('image'), adminController.postRestaurant)
   app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
   app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
   app.put('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
   app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
+
   app.get('/admin/users', authenticatedAdmin, adminController.getUsers)
   app.put('/admin/users/:id/toggleAdmin', authenticatedAdmin, adminController.putUsers)
+
   app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
   app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
   app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
