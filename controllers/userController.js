af88ce1976ac7bcbd07a4657af8b83b379274512
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-12-04 12:38:00 +0800

    Add favorite/unfavorite feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,6 +7,7 @@ const db = require('../models')
 const User = db.User
 const Comment = db.Comment
 const Restaurant = db.Restaurant
+const Favorite = db.Favorite
 
 const userController = {
   signUpPage: (req, res) => {
@@ -117,6 +118,26 @@ const userController = {
         })
         .catch(err => next(err))
     }
+  },
+  addFavorite: (req, res) => {
+    return Favorite.create({
+      UserId: req.user.id,
+      RestaurantId: req.params.restaurantId
+    }).then(restaurant => {
+      return res.redirect('back')
+    })
+  },
+  removeFavorite: (req, res) => {
+    return Favorite.findOne({
+      where: {
+        UserId: req.user.id,
+        RestaurantId: req.params.restaurantId
+      }
+    }).then(favorite => {
+      favorite.destroy().then(restaurant => {
+        return res.redirect('back')
+      })
+    })
   }
 }
 
